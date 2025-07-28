70f931335540c128995b706440e07882632d8acf
1801ritik <1801rkm@gmail.com>
2025-07-28 13:15:30 +0530

    Add .next/static/chunks/src_app_page_tsx_a993527f._.js 1753688729064

---
+++ .next/static/chunks/src_app_page_tsx_a993527f._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/src_d99833c8._.js",
+  "static/chunks/node_modules_189af304._.js"
+],
+    source: "dynamic"
+});
